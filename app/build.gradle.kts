dc7dc11caa373a81c34eab09f4971c80fae46e53
qianxu <qianxuuuu@qq.com>
2026-03-16 22:52:06 +0800

    更新依赖版本，移除无用的依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   alias(libs.plugins.android.application)
-  alias(libs.plugins.jetbrains.kotlin.android)
   alias(libs.plugins.kotlin.compose)
 }
 
@@ -27,12 +26,12 @@ android {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
-  kotlin {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-    }
-  }
   buildFeatures { compose = true }
+  compileSdkMinor = 1
+}
+
+kotlin {
+  jvmToolchain(17)
 }
 
 dependencies {
@@ -43,5 +42,4 @@ dependencies {
   implementation(libs.androidx.ui.graphics)
   implementation(libs.androidx.material3)
   implementation(libs.coil.compose)
-  implementation(libs.androidx.appcompat)
 }
